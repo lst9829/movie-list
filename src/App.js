853a8dd83f411648587a3c390cc873c197380172
Louis <lst9829@yahoo.com>
2023-11-06 15:29:07 -0700

    added button to toggle watched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 
+let watchedArray = [];
+let toWatchArray = [];
+
 function App() {
   const[movies, setMovies] = useState([{
     id: 0,
@@ -10,6 +13,8 @@ function App() {
   const[results, setResults] = useState();
   const[clicked, setClicked] = useState(false);
   const[newMovie, setNewMovie] = useState("");
+  const[watchedClick, setWatchedClick] = useState(false);
+  const[toWatchClick, setToWatchClick] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:8000/movies')
@@ -51,22 +56,53 @@ function App() {
       body: JSON.stringify(jsonData)
     })
   }
-  return (
-    <div className="App">
-      {clicked === false ?
+
+  const handleWatched = (checked, value) => {
+    if (checked === true){
+      watchedArray.push(value)
+      let toWatch = toWatchArray.filter(movie => movie !== value)
+      toWatchArray = toWatch
+    } else {
+      let filtered = watchedArray.filter(movie => movie !== value)
+      watchedArray = filtered
+      toWatchArray.push(value)
+    }
+    console.log(watchedArray)
+  }
+
+  const handleWatchedList = () => {
+    setWatchedClick(true);
+  }
+
+  const handleToWatchList = () => {
+    setToWatchClick(true)
+  }
+
+  const buttonPresses = () => {
+    if(clicked === false && watchedClick === false && toWatchClick === false){
+      return (
       <>
+        <button type="submit" onClick={() => handleWatchedList()}>Watched List</button>
+        <button type="submit" onClick={() => handleToWatchList()}>To Watch List</button>
         <input type="search" value={search} placeholder="search for movie" onChange={(event) => setSearch(event.target.value)}></input>
         <button type="submit" onClick={() => handleSearch()}>Search</button>
         <input type="text" value={newMovie} placeholder="add new movie" onChange={(event) => setNewMovie(event.target.value)}></input>
         <button type="submit" onClick={() => handleAdd()}>Add New Movie</button>
-        {movies.map(movie => (
+        {movies.map((movie) => (
           <div key={movie.id}>
             <div>{movie.title}</div>
             <button type='submit' onClick={() => handleDelete(movie.title)}>Delete</button>
+            <label>
+              <input type='checkbox' value={movie.title} onChange={(event) => handleWatched(event.target.checked, event.target.value)}/>
+              Watched
+            </label>
           </div>
         ))}
-      </> :
-      <div>
+      </>
+      )
+    } else if (clicked === true && watchedClick === false && toWatchClick === false){
+      return (
+        <div>
           <>
             <button type='submit' onClick={() => setClicked(false)}>Go Home</button>
             {results.map(movie => (
@@ -75,8 +111,48 @@ function App() {
               </>
             ))}
           </>
-      </div>
-      }
+        </div>
+      )
+    } else if (clicked === false && watchedClick === true && toWatchClick === false){
+      return (
+        <div>
+          <>
+            <button type='submit' onClick={() => {
+              watchedArray = [];
+              toWatchArray = [];
+              setWatchedClick(false)}}>Go Home</button>
+            {watchedArray.map(movie => (
+              <>
+                <div>{movie}</div>
+              </>
+            ))}
+          </>
+        </div>
+      )
+    } else if (clicked === false && watchedClick === false && toWatchClick === true){
+      return(
+        <div>
+          <>
+            <button type='submit' onClick={() => {
+              watchedArray = [];
+              toWatchArray = [];
+              setToWatchClick(false)}}>Go Home</button>
+            {toWatchArray.map(movie => (
+              <>
+                <div>{movie}</div>
+              </>
+            ))}
+          </>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className="App">
+      <>
+        <div>{buttonPresses()}</div>
+      </>
     </div>
   );
 }
